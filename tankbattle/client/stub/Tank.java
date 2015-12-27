466d49c248f35d034e03733e797e32d56b3f848b
Megasonic988 <weixiang@ualberta.ca>
2015-12-27 12:32:43 -0700

    fixing dodge

--- tankbattle/client/stub/Tank.java
+++ tankbattle/client/stub/Tank.java
@@ -132,7 +132,8 @@ public class Tank {
                 double perp_direction1 = p.direction + Math.PI / 2;
                 double perp_direction2 = p.direction - Math.PI / 2;
                 double difference = Math.min(this_tank.direction - perp_direction1, this_tank.direction - perp_direction2);
-                System.out.println(tankID + direction);
+                System.out.println(tankID + " " + this_tank.direction);
+                System.out.println(p.direction);
                 String rotate_tracks_command = command.rotate(tankID, "CW", difference, gameInfo.getClientToken());
                 commands.add(rotate_tracks_command);
 
