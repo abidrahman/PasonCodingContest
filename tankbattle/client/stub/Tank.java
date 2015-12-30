f99cf8ef4344ab1d33ac0744369ee0acc81b9e0b
Abid Rahman <arahman1@ualberta.ca>
2015-12-30 16:06:44 -0700

    Update Tank.java

--- tankbattle/client/stub/Tank.java
+++ tankbattle/client/stub/Tank.java
@@ -216,16 +216,17 @@ public class Tank {
             JSONObject map = gameState.getJSONObject("map");
             JSONArray terrains = map.getJSONArray("terrain");
             for (int i = 0; i < terrains.length(); i++) {
+                
+                JSONObject obstacle = terrains.getJSONObject(i);
+                if (obstacle.getString("type").equals("SOLID")) {
 
-                if (terrains.getJSONObject(i).getString("type").equals("SOLID")) {
-
-                    JSONObject obstacle = terrains.getJSONObject(i);
+                    JSONObject bounds = obstacle.getJSONObject("boundingBox");
                     Vector obs_start = new Vector();
                     Vector obs_end = new Vector();
-                    obs_start.x = (double)obstacle.getJSONArray("corner").getInt(0);
-                    obs_start.y = (double)obstacle.getJSONArray("corner").getInt(1);
-                    obs_end.x = obs_start.x + (double)obstacle.getJSONArray("size").getInt(0);
-                    obs_end.y = obs_start.y + (double)obstacle.getJSONArray("size").getInt(1);
+                    obs_start.x = (double)bounds.getJSONArray("corner").getInt(0);
+                    obs_start.y = (double)bounds.getJSONArray("corner").getInt(1);
+                    obs_end.x = obs_start.x + (double)bounds.getJSONArray("size").getInt(0);
+                    obs_end.y = obs_start.y + (double)bounds.getJSONArray("size").getInt(1);
 
                     if (Line2D.linesIntersect(start.x,start.y,end.x,end.y,obs_start.x,obs_start.y,obs_end.x,obs_end.y) ||
                             Line2D.linesIntersect(start.x,start.y,end.x,end.y,obs_end.x,obs_start.y,obs_start.x,obs_end.y)) {
