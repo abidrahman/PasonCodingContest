ed58f77a4a8be9a575f959e93b8e2d61d61c5275
Megasonic988 <weixiang@ualberta.ca>
2015-12-31 12:10:05 -0700

    pathfinding

--- tankbattle/client/stub/Tank.java
+++ tankbattle/client/stub/Tank.java
@@ -103,7 +103,7 @@ public class Tank {
             }
         }
 
-        if (closest.x != 0 && closest.y != 0) state = State.DOGFIGHT;
+        if (closest.x > 0.1 && closest.y > 0.1) state = State.DOGFIGHT;
         else state = State.HUNTING;
 
         if (state == State.DOGFIGHT) {
