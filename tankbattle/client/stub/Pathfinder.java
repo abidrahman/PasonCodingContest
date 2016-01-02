5b619937129f188f118b0395f35ebbbcd807ced8
Megasonic988 <weixiang@ualberta.ca>
2016-01-02 11:04:36 -0700

    pathfinding

--- tankbattle/client/stub/Pathfinder.java
+++ tankbattle/client/stub/Pathfinder.java
@@ -94,8 +94,8 @@ public class Pathfinder {
                 int width = bounds.getJSONArray("size").getInt(0);
                 int height = bounds.getJSONArray("size").getInt(1);
                 System.out.println("corner_x: " + corner_x + ", corner_y: " + corner_y + ", width: " + width + ", height: " + height);
-                for (int y = -2; y < height+2; y++) {
-                    for (int x = -2; x < width+2; x++) {
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
                         if (corner_x + x < 0 || !(corner_x + x < this.map.map_width) || corner_y + y < 0 || !(corner_y + y < this.map.map_height)) continue;
                         Node n = this.map.getNode(corner_x + x, corner_y + y);
                         n.impassable = true;
