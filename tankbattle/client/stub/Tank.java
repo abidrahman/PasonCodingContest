46f87f27d6271fb768f490ea6d8742f961df1714
Megasonic988 <weixiang@ualberta.ca>
2016-01-09 15:00:15 -0700

    pathfinding

--- tankbattle/client/stub/Tank.java
+++ tankbattle/client/stub/Tank.java
@@ -168,12 +168,11 @@ public class Tank {
 
         if (state == State.HUNTING) {
 
-            /*
+
             state = State.DOGFIGHT;
             return commands;
-            */
 
-
+            /*
             count++;
             System.out.println(count);
             commands.addAll(dodgeProjectiles());
@@ -185,7 +184,7 @@ public class Tank {
             if (count % 25 == 1) {
                 commands.addAll(huntEnemy());
             }
-
+            */
         }
 
         return commands;
